89549a7c3c8777f447e22b0107107725d588c2f7
aHasnat1997 <hasnat56703@gmail.com>
2023-05-18 19:33:19 +0600

    all products and single product api done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,7 +17,6 @@ app.get('/', (req, res) => {
 // mongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@heroic-toys-db.fvxtohq.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -31,12 +30,42 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection = client.db('heroic-toys').collection('products');
+
+    app.get('/all-products', async (req, res) => {
+      const result = await productCollection.find().project({ name: 1, image: 1, price: 1, rating: 1 }).toArray();
+      res.send(result);
+    })
+
+    app.get('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+
+    // app.get('/products/:featured', async (req, res) => {
+    //   const featured = req.params.featured;
+    //   // console.log(featured);
+    //   const query = { featuredAs: new ObjectId(featured) };
+    //   // console.log(query);
+    //   const result = await productCollection.find(query).project({ name: 1, image: 1, price: 1, rating: 1 }).toArray();
+    //   res.send(result);
+    // });
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
